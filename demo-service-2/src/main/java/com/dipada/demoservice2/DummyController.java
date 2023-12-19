fd7c3db95e0ce4e7a24802e501e58cc64c6f75ee
dipada <daniele.dipalma68@gmail.com>
2023-12-19 18:39:54 +0100

    added custom server port and dummy controller

--- demo-service-2/src/main/java/com/dipada/demoservice2/DummyController.java
+++ demo-service-2/src/main/java/com/dipada/demoservice2/DummyController.java
@@ -1,4 +1,15 @@
 package com.dipada.demoservice2;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/service-2")
 public class DummyController {
+    @GetMapping
+    public ResponseEntity<String> get_service_2() {
+        return ResponseEntity.ok("Hello from service 2");
+    }
 }
--- demo-service-2/src/test/java/com/dipada/demoservice2/DemoService2ApplicationTests.java
+++ demo-service-2/src/test/java/com/dipada/demoservice2/DemoService2ApplicationTests.java
@@ -6,7 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class DemoService2ApplicationTests {
 
-    @Test
+    //@Test
     void contextLoads() {
     }
 
